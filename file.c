674bbc1845a72fd81e7c5d1919df74177cbc3ba3
AbdElrahman I.Zaki <abdelrahmanzaki.aez@gmail.com>
2021-07-08 20:22:04 +0200

    test_remote_repo first commit

--- file.c
+++ file.c
@@ -3,7 +3,6 @@ void main(void)
 {
 	printf("This is my first commit");
 	printf("This is my second commit");
-	printf("This is first line in feature_1");
-	printf("This is first line in feature_2");
+	
 }
 /* EOC */
